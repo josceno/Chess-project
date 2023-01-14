847bec6790268f66618c3679e7356d54dacf8ef5
josceno <jdamasceno414@gmail.com>
2023-01-14 11:45:47 -0300

    First Class Position (182)

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package src.application;
+
+import src.boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position position = new Position(2,3);
+        System.out.println(position);
+    }
+}
